f28664c1c6c13c77394c4c67a40acc64684404ba
DucCuong <lamduccuong123@gmail.com>
2022-10-01 22:36:04 +0700

    Cau 3

--- src/main/java/com/example/tuan4/controller/NhanVienController.java
+++ src/main/java/com/example/tuan4/controller/NhanVienController.java
@@ -1,11 +1,27 @@
 package com.example.tuan4.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/maybay")
+import com.example.tuan4.entity.NhanVien;
+import com.example.tuan4.repository.NhanVienRepository;
+
+@RequestMapping("/nhanvien")
 @RestController
 public class NhanVienController {
 
+	@Autowired
+	NhanVienRepository nhanVienRepository;
+
+	// 3. Tìm các nhân viên có lương nhỏ hơn 10,000.
+	@GetMapping("/nhanVienLuongDuoi10000")
+	public List<NhanVien> nhanVienLuongDuoi10000() {
+		return nhanVienRepository.findNhanVienbyLuongNhoHon10000();
+
+	}
 }
--- src/main/java/com/example/tuan4/repository/NhanVienRepository.java
+++ src/main/java/com/example/tuan4/repository/NhanVienRepository.java
@@ -1,6 +1,9 @@
 package com.example.tuan4.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.tuan4.entity.NhanVien;
@@ -8,4 +11,6 @@ import com.example.tuan4.entity.NhanVien;
 @Repository
 public interface NhanVienRepository extends JpaRepository<NhanVien, String> {
 
+	@Query(value = "select * from nhanvien where Luong < 10000", nativeQuery = true)
+	List<NhanVien> findNhanVienbyLuongNhoHon10000();
 }
